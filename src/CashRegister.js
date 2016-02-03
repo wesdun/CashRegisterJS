0f8306b8b857e51c91c7a2df2bb0fe2f5fb260cd
Wesley Duncan <duncan.wesley@gmail.com>
2016-02-02 22:52:41 -0500

    test setup; drawer function created

---
+++ src/CashRegister.js
@@ -0,0 +1,7 @@
+function drawer (price, cash, cid) {
+    var change;
+    change = "Closed";
+    return change;
+}
+
+module.exports = drawer;
--- test/CashRegister.Test.js
+++ test/CashRegister.Test.js
@@ -1,4 +1,18 @@
 /**
  * Created by w_dun on 2/2/2016.
  */
+var assert = require('assert');
+var drawer = require('../src/CashRegister.js');
+
+it('should have a drawer method', function(){
+    //assert.equal(typeof cr, 'object');
+    assert.equal(typeof drawer, 'function');
+});
+
+it('should return Closed when price same as cash', function() {
+
+    assert.equal(drawer(0, 0, []), "Closed");
+});
+
+
 
